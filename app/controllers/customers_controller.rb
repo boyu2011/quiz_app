9c13f08a65d0598a6f083beb9924f4dedec6962d
boyu2011 <boyu2011@gmail.com>
2013-02-25 21:24:53 -0500

    finish customer management

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,11 +1,19 @@
 class CustomersController < ApplicationController
+  
+  before_filter :signed_in_admin
+  
   def create
     @customer = Customer.new(params[:customer])
+    @customer.quiz_score = params[:quiz_score]
     if @customer.save
       redirect_to :action => "show"
     else
       redirect_to :action => "fail"
     end
+  end
+  
+  def index
+    @customers = Customer.all
   end
   
   def show
@@ -15,5 +23,10 @@ class CustomersController < ApplicationController
   def fail
     
   end
-
+  
+  private
+    
+    def signed_in_admin
+      redirect_to signin_url, notice: "Please sign in." unless signed_in?
+    end
 end
